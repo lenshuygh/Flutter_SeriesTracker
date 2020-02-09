2b35a93a93f531ec8e56fad463bab4676b1303f9
lens <lens.huygh@gmail.com>
2020-02-09 21:40:39 +0100

    refactor creation of items in a separate method

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:toast/toast.dart';
 
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -12,15 +11,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Series Tracker',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primaryColor: Colors.black,
       ),
       home: MyHomePage(title: 'Series Tracker'),
@@ -28,19 +18,8 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -48,8 +27,18 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final List<ListTile> _seriesTitles = <ListTile>[];
   final dateFormat = new DateFormat('dd/MM/yyyy');
   int _counter = 0;
+
+  void _fillSeriesTitles() {
+    _seriesTitles.add(_title('Star Trek: Picard', DateTime.parse("2020-01-23"),
+        "assets/images/icons/picard.jpg"));
+    _seriesTitles.add(_title('The New Pope', DateTime.parse("2020-01-23"),
+        "assets/images/icons/theNewPope.jpg"));
+    _seriesTitles.add(_title('Westworld', DateTime.parse("2016-10-02"),
+        "assets/images/icons/westworld.jpg"));
+  }
 
   void _incrementCounter() {
     setState(() {
@@ -59,7 +48,7 @@ class _MyHomePageState extends State<MyHomePage> {
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       _counter++;
-      print('CLICK'+ _counter.toString());
+      print('CLICK' + _counter.toString());
     });
   }
 
@@ -71,13 +60,15 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
+    _fillSeriesTitles();
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: _buildSeriesList(
+      body: ListView(
+          children: _seriesTitles
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
@@ -87,38 +78,27 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  Widget _buildSeriesList() =>
-      ListView(
-        children: [
-          _title('Star Trek: Picard', DateTime.parse("2020-01-23"),
-              "assets/images/icons/picard.jpg"),
-          _title('The New Pope', DateTime.parse("2020-01-23"),
-              "assets/images/icons/theNewPope.jpg"),
-          _title('Westworld', DateTime.parse("2016-10-02"),
-              "assets/images/icons/westworld.jpg")
-        ],
-      );
-
   ListTile _title(String title, DateTime startDate, String imageUrl) =>
       ListTile(
-        title: Text(
-            title,
+        title: Text(title,
             style: TextStyle(
               fontWeight: FontWeight.w500,
               fontSize: 20,
             )),
-        subtitle: Text(dateFormat.format(startDate),style: TextStyle(fontSize: 11,)),
+        subtitle: Text(dateFormat.format(startDate),
+            style: TextStyle(
+              fontSize: 11,
+            )),
         leading: CircleAvatar(backgroundImage: AssetImage(imageUrl)),
-        onTap: (){
+        onTap: () {
           /*setState(() {
             print(title);
           });*/
-          Toast.show("Clicked on " + title, context, duration: Toast.LENGTH_SHORT, gravity:  Toast.BOTTOM);
+          Toast.show("Clicked on " + title, context,
+              duration: Toast.LENGTH_SHORT, gravity: Toast.BOTTOM);
         },
         onLongPress: () {
-          setState(() {
-
-          });
+          setState(() {});
         },
       );
 }
